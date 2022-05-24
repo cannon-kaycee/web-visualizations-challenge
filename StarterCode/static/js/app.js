c77f764bece3b59ede582eb3ffa84afe5f68b1fd
KayCan <cannon.kaycee@gmail.com>
2022-05-23 19:27:54 -0700

    second

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,6 @@
 // Fetch the JSON data and console log it
 const biodata = "../data/samples.json"
+
 function buildMetadata(sample) {  
     d3.json("samples.json").then((biodata) => {    
         var metadata = biodata.metadata;
@@ -13,24 +14,32 @@ function buildMetadata(sample) {
         // Hint: Inside the loop, you will need to use d3 to append new
         // tags for each key-value in the metadata.
         Object.entries(result).forEach(([key, value]) => {
-            PANEL.append("h6").text(${key.toUpperCase()}:${value});
+            PANEL.append("h6").text(`${key.toUpperCase()}:${value}`);
         });
         // BONUS: Build the Gauge Chart
         buildGauge(result.wfreq);
     });
 }
 
+//Start the dropdown
+d3.selectAll("#selDataset").on("change", dropdown);
 
-
-  var otu = d3.select("otu_ids").text();
-  console.log("text1 says: ", otu);
-
-// This function is called when a dropdown menu item is selected, Use D3 to select the dropdown menu, 
-// Assign the value of the dropdown menu option to a variable
 function dropdown() {
     var dropdownMenu = d3.select("#selDataset");
     var dataset = dropdownMenu.property("value");
+    var id_num = sample.names;
+
+
 }
+
+
+
+
+
+
+
+
+
 
 
 // Bubble
