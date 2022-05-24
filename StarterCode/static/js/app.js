7303ec7ae0d6d125f311ffbc7cf1b8ec0bef91df
KayCan <cannon.kaycee@gmail.com>
2022-05-23 17:58:49 -0700

    first

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,35 @@
 // Fetch the JSON data and console log it
 const biodata = "../data/samples.json"
-d3.json(biodata).then(function(data) {
-    console.log(data);
-  });
+function buildMetadata(sample) {  
+    d3.json("samples.json").then((biodata) => {    
+        var metadata = biodata.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);    
+        var result = resultArray[0];    
+        // Use d3 to select the panel with id of #sample-metadata    
+        var PANEL = d3.select("#sample-metadata");
+        // Use `.html("") to clear any existing metadata
+        PANEL.html("");
+        // Use Object.entries to add each key and value pair to the panel
+        // Hint: Inside the loop, you will need to use d3 to append new
+        // tags for each key-value in the metadata.
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(${key.toUpperCase()}:${value});
+        });
+        // BONUS: Build the Gauge Chart
+        buildGauge(result.wfreq);
+    });
+}
+
+
+
   var otu = d3.select("otu_ids").text();
   console.log("text1 says: ", otu);
 
 // This function is called when a dropdown menu item is selected, Use D3 to select the dropdown menu, 
 // Assign the value of the dropdown menu option to a variable
-function optionChanged() {
+function dropdown() {
     var dropdownMenu = d3.select("#selDataset");
-    var dataset = dropdownMenu.property("sample_values");
+    var dataset = dropdownMenu.property("value");
 }
 
 
